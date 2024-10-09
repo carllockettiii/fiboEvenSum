01a8736017ad9774320c1b2ef8b6a577192145a9
Carl Lockett <carllockett3@gmail.com>
2024-10-08 23:29:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int FiboEvenSum(int number)
+{
+    int x = 0;
+    int numberOne = 0;
+    int numberTwo = 1;
+    int counter = 0;
+    int tempNumber = 0;
+
+    while (x < number)
+    {
+        tempNumber = numberTwo + numberOne;
+        numberOne = numberTwo;
+        numberTwo = tempNumber;
+        x = tempNumber;
+
+        if ((x % 2) == 0)
+        {
+            counter += x;
+        }
+    }
+    return counter;
+}
+
+int fib = 100000;
+int result = FiboEvenSum(fib);
+Console.WriteLine("Fibonacci even number of " + fib + " is " + result);
